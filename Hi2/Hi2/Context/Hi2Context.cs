c1b697d1bd703a505850ca968b1c1b8f86ce3b1c
Diana001a <di89101630730@mail.ru>
2021-09-30 19:30:36 +0300

    написали код
    написали код, подключили к серверу

---
+++ Hi2/Hi2/Context/Hi2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Hi2.Models;
+
+namespace Hi2.Context
+{
+    public class Hi2Context : DbContext
+    {
+        public Hi2Context (DbContextOptions<Hi2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NoteY> NoteY { get; set; }
+    }
+}
